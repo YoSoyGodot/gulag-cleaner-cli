b34aedd3568ae5041db4239c31b6cc13da5125a1
YoSoygodot <pablotoresrodriguez@gmail.com>
2022-09-28 10:47:23 +0200

    4.1.6
    se ha rehecho el sistema de argumentos

--- gulagcleaner/command_line.py
+++ gulagcleaner/command_line.py
@@ -1,5 +1,7 @@
-from gulagcleaner import gulagcleaner_extract
+#from gulagcleaner import gulagcleaner_extract
+import gulagcleaner_extract
 from os.path import exists
+import argparse
 
 def main():
     '''
@@ -8,47 +10,37 @@ def main():
     The pages are saved in a new PDF in the same folder.
     '''
     
-    import sys
-    opt = ['-h','-r','-v']
-    if '-h' in sys.argv:
-        print("available arguments:")
-        print("-h : displays info")
-        print("-r : replaces original file")
-        print("-v : shows version of the program")
-    else:
-        if '-v' in sys.argv:
-            print("actual version: 0.4.4")
-        else:
-            
-            if len(sys.argv)>1:
-                if '-r' in sys.argv:
-                    replace = True
-                    sys.argv.remove('-r')
+    import sys        
+    if len(sys.argv)>1:
+        replace = False
+        parser = argparse.ArgumentParser(prog="GulagCleaner",
+                            description='Eliminador de anuncios de pdfs de Wuolah.')
+        parser.add_argument('-v', action='versión', version='%(prog)s 4.1.6')
+        parser.add_argument('-r', dest='reemplazar', action='store_true',
+                            help='reemplaza el pdf original')        
+
+        args = parser.parse_args()
+        for arg in args:
+            if exists(arg):
+                return_msg=gulagcleaner_extract.deembed(arg,parser.)
+                if return_msg["Success"]:
+                    print("Deembedding successful. File saved in",return_msg["return_path"])
+                    print("Metadata:")
+                    print("Archivo: "+return_msg["Meta"]["Archivo"])
+                    print("Autor: "+return_msg["Meta"]["Autor"])
+                    print("Asignatura: "+return_msg["Meta"]["Asignatura"])
+                    print("Curso y Grado: "+return_msg["Meta"]["Curso y Grado"])
+                    print("Facultad: "+return_msg["Meta"]["Facultad"])
+                    print("Universidad: "+return_msg["Meta"]["Universidad"])
+
                 else:
-                    replace = False
-                del sys.argv[0]
-                for arg in sys.argv:
-                    if exists(arg):
-                        return_msg=gulagcleaner_extract.deembed(arg,replace)
-                        
-                        if return_msg["Success"]:
-                            print("Deembedding successful. File saved in",return_msg["return_path"])
-                            print("Metadata:")
-                            print("Archivo: "+return_msg["Meta"]["Archivo"])
-                            print("Autor: "+return_msg["Meta"]["Autor"])
-                            print("Asignatura: "+return_msg["Meta"]["Asignatura"])
-                            print("Curso y Grado: "+return_msg["Meta"]["Curso y Grado"])
-                            print("Facultad: "+return_msg["Meta"]["Facultad"])
-                            print("Universidad: "+return_msg["Meta"]["Universidad"])
-                            
-                        else:
-                            print("Error:",return_msg["Error"])
-                    else:
-                        print("File not found.")
-                else:
-                    pass
+                    print("Error:",return_msg["Error"])
             else:
-                print('Usage: gulagcleaner "filename". Type "gulagcleaner -h" for more information.')
+                print("File not found.")
+        else:
+            pass
+    else:
+        print('Usage: gulagcleaner "filename". Type "gulagcleaner -h" for more information.')
 
 if __name__ == "__main__":
     print('Call from the "gulagcleaner" command.')
